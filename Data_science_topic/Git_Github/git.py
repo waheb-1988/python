1c2c25504fb69b0a43a3384a5806c239e71b6c1a
waheb-1988 <hocineabdelouaheb@yahoo.fr>
2022-10-09 15:26:45 +0100

    new structure

--- Data_science_topic/Git_Github/git.py
+++ Data_science_topic/Git_Github/git.py
@@ -1,4 +1,5 @@
 from cmath import log
+from http.client import RESET_CONTENT
 
 
 git diff -r HEAD data/northern.csv
@@ -17,9 +18,20 @@ If you accidentally mistype a commit message, you can change it using the --amen
 
 git commit --amend - m "new message"
 
+
 git commit -m "Adding a reference."
 
 
 git log
  space + q to exit
  
+ git show (first caracter of hash code)
+ 
+ to show the comist before the most Rrecent
+ 
+ git diff -r HEAD~1
+ 
+ git annotate file shows who made the last change to each line of a file and when.
+ 
+ 
+ git diff HEAD..HEAD~1
